a65d03d46b874865ce71e799861728e32023d963
Teodora Vodopija <115836182+TeodoraVodopija@users.noreply.github.com>
2023-01-11 17:05:39 +0100

    micro1

--- mic0.py
+++ mic0.py
@@ -3,49 +3,48 @@ import aiofiles
 import aiosqlite
 from aiohttp import web
 import sqlite3
-import requests
 
-conn = sqlite3.connect('base.db')
-cursor = conn.cursor()
+connection = sqlite3.connect('base.db')
+cursor = connection.cursor()
 
-cursor.execute("SELECT COUNT(*) FROM base")
-cf = cursor.fetchone()[0]
+cursor.execute("SELECT COUNT(*) FROM project_base")
+c = cursor.fetchone()[0]
 
-if cf == 0:
+if c == 0:
     print("The database is empty.")
 else:
-    print("The database not empty.")
+    print("The database is not empty.")
 
 routes = web.RouteTableDef()
 
 
 @routes.get("/dataJson")
-async def data_json(req):
+async def data_json(request):
     async with aiofiles.open('file-000000000040.json', mode='r') as file_data:
-        r_data = {await file_data.readline() for _ in range(10)}
-        data = [json.loads(line) for line in r_data]
-        db = []
-        result = []
+        data_read = {await file_data.readline() for _ in range(10)}
+        all_data = [json.loads(line) for line in data_read]
+        database = []
+        final = []
         async with aiosqlite.connect("base.db") as db:
-            for item in data:
-                db_item = {}
-                db_item["username"] = item["repo_name"].rsplit("/", 1)[0]
-                db_item["ghlink"] = "https://github.com/" + item["repo_name"] + ".com"
-                path = item["path"].rsplit("/", 1)
-                if len(path) > 1:
-                    db_item["filename"] = path[1]
+            for item in all_data:
+                items = {}
+                items["username"] = item["repo_name"].rsplit("/", 1)[0]
+                items["ghlink"] = "https://github.com/" + item["repo_name"] + ".com"
+                path_parts = item["path"].rsplit("/", 1)
+                if len(path_parts) > 1:
+                    items["filename"] = path_parts[1]
                 else:
-                    db_item["filename"] = item["path"]
+                    items["filename"] = item["path"]
 
-                db.append(db_item)
-                await db.execute("INSERT INTO base (username, ghlink, filename) VALUES (?,?,?)",
-                                (db_item["username"], db_item["ghlink"], db_item["filename"]))
-            async with db.execute("SELECT * FROM base LIMIT 100") as cur:
-                col = [column[0] for column in cur.description]
+                database.append(items)
+                await db.execute("INSERT INTO project_base (username, ghlink, filename) VALUES (?,?,?)",
+                                 (items["username"], items["ghlink"], items["filename"]))
+            async with db.execute("SELECT * FROM project_base LIMIT 100") as cur:
+                columns = [column[0] for column in cur.description]
                 result = await cur.fetchall()
                 for row in result:
-                    result.append(dict(zip(col, row)))
-                data = result
+                    final.append(dict(zip(columns, row)))
+                data = final
 
                 await db.commit()
         return web.json_response(data, status=200)
